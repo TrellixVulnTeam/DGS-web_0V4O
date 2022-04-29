0ec1e4db4770216c743a934fc229d99bfaf72602
Pico-Dev <mclartydan0505@gmail.com>
2022-04-29 17:51:42 -0400

    Added Game Link

--- src/components/Home/pages/About.js
+++ src/components/Home/pages/About.js
@@ -7,7 +7,8 @@ function About() {
     <>
     <VideoSection />
     {/* <AboutYoutubeVideo /> */}
-   
+          <center><h1>Soul Runner Game Link</h1></center>
+          <center><iframe src="https://itch.io/embed/1508229?dark=true" width="552" height="167" frameborder="0"><a href="https://danielgamestudios.itch.io/soul-runner">Soul Runner by Daniel Games</a></iframe></center>
     </>
   )
 }
--- src/components/Home/pages/News.js
+++ src/components/Home/pages/News.js
@@ -5,8 +5,10 @@ import VideoSection from '../VideoSection/VideoSection'
 function News() {
   return (
     <div>
-        <VideoSection />
-        <NewsPost />
+          <VideoSection />
+          <center><h1>Soul Runner Game Link</h1></center>
+          <center><iframe src="https://itch.io/embed/1508229?dark=true" width="552" height="167" frameborder="0"><a href="https://danielgamestudios.itch.io/soul-runner">Soul Runner by Daniel Games</a></iframe></center>
+          <NewsPost />
     </div>
   )
 }
